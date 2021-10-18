ba2044582d582c04d7f77867146bff040115098d
Gaywiz <62390166+anmsg98@users.noreply.github.com>
2021-10-18 13:56:16 +0900

    10
    drill #10

--- boy_grass_object.py
+++ boy_grass_object.py
@@ -7,6 +7,42 @@ class Grass:
     def draw(self):
         self.image.draw(400,30)
 
+
+class Ball:
+    def __init__(self):
+        self.x, self.y = random.randint(0, 700), 599  # 위치 정함
+        self.image = load_image('ball21x21.png')
+        self.frame = 0
+
+    def update(self):
+        t = random.randint(1,5)
+        self.y -= t
+        if self.y<=50:
+            self.y=50
+
+        self.frame = (self.frame + 1) % 8
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+class Ball2:
+    def __init__(self):
+        self.x, self.y = random.randint(0, 700), 599  # 위치 정함
+        self.image = load_image('ball41x41.png')
+        self.frame = 0
+
+    def update(self):
+        t = random.randint(1, 6)
+        self.y -= t
+        if self.y<=50:
+            self.y=50
+
+        self.frame = (self.frame + 1) % 8
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+
 class Boy:
     def __init__(self):
         self.x, self.y = random.randint(100,700), 90  #위치 정함
@@ -36,7 +72,8 @@ grass = Grass()
 
 team = [Boy() for i in range(1,11+1)]
 
-
+balls = [Ball() for i in range(1,11)]
+balls2 = [Ball2() for i in range(1,11)]
 
 running = True
 # game main loop code
@@ -46,6 +83,14 @@ while running:
     #game logic
     for boy in team:
         boy.update()
+
+    for ball in balls:
+        ball.update()
+
+    for ball2 in balls2:
+        ball2.update()
+
+
     #boy.update()
 
 
@@ -54,9 +99,18 @@ while running:
     grass.draw()
     for boy in team:
         boy.draw()
+
+    for ball in balls:
+        ball.draw()
+
+    for ball2 in balls2:
+        ball2.draw()
+
+
+
     update_canvas()
 
-    delay(0.05)
+    #delay(0.05)
 
 
 # finalization code
